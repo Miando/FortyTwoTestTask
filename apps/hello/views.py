d99f786d9ef5d8bf1b4cbcfaef6db3ab03503b18
Mykhailo <kuznecov513@gmail.com>
2017-04-18 18:07:15 +0300

    ticket:1 added template, view, url, model

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Test
+from .models import Person
 
 
 def index(request):
-    test = Test.objects.all()
-    context = {'test': test}
-    return render(request, 'person/index.html', context)
+    person = Person.objects.get(name="Misha")
+    context = {'person': person}
+    return render(request, 'hello/index.html', context)
