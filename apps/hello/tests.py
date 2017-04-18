762fc10ea409ec81868dca68a8655db9530d3a53
Mykhailo <kuznecov513@gmail.com>
2017-04-18 22:34:30 +0300

    ticket:1 added tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .models import Person
 
 
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+
+
+class TestPerson(TestCase):
+    def test_person_data(self):
+        person = Person.objects.get(name="Misha")
+        self.assertEquals(person.name, "Misha")
+        self.assertEquals(person.last_name, "Kuznietsov")
+        self.assertEquals(person.email, "kuznecov513@gmail.com")
+        self.assertEquals(person.jabber, "miando@42cc.co")
