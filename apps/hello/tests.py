efec999c93e095b7300702eaf8a5b7b6a7a56f77
Mykhailo <kuznecov513@gmail.com>
2017-04-20 19:00:26 +0300

    ticket:1 update tests via plan

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Person
+from .models import Person, RequestModel
 
 
 class SomeTests(TestCase):
@@ -8,7 +8,7 @@ class SomeTests(TestCase):
         assert(2 + 2 == 4)
 
 
-class TestPerson(TestCase):
+class TestModel(TestCase):
     def test_person_data(self):
         "test of adding data to Person model"
         person = Person.objects.get(name="Misha")
@@ -17,14 +17,32 @@ class TestPerson(TestCase):
         self.assertEquals(person.email, "kuznecov513@gmail.com")
         self.assertEquals(person.jabber, "miando@42cc.co")
 
+    def test_request_model(self):
+        "cheking middleware"
+        self.client.get('/request/')
+        req = RequestModel.objects.latest("id")
+        self.assertEquals(req.url, "/request/")
+
 
 class ProjectTests(TestCase):
 
-    def test_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_status_200(self):
+        "test status code"
+        requests = [
+            '/',
+            '/request/',
+        ]
+        for request in requests:
+            response = self.client.get(request)
+            self.assertEqual(response.status_code, 200)
 
-    def test_template(self):
+    def tests_template_index(self):
+        "test for index template"
         response = self.client.get('/')
         self.assertContains(response, 'title')
         self.assertContains(response, 'body')
+
+    def tests_template_requests(self):
+        "test fo last_request template"
+        response = self.client.get('/request/')
+        self.assertContains(response, '/request/')
