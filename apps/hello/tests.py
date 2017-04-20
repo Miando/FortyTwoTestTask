70f956fbbaa1fc660be488ab93c6825242732e43
Mykhailo <kuznecov513@gmail.com>
2017-04-20 18:07:52 +0300

    ticket:1 added and fixed test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -11,14 +11,20 @@ class SomeTests(TestCase):
 class TestPerson(TestCase):
     def test_person_data(self):
         "test of adding data to Person model"
-        person = Person()
-        person.name = "Misha"
-        person.last_name = "Kuznietsov"
-        person.email = "kuznecov513@gmail.com"
-        person.jabber = "miando@42cc.co"
-        person.save()
         person = Person.objects.get(name="Misha")
         self.assertEquals(person.name, "Misha")
         self.assertEquals(person.last_name, "Kuznietsov")
         self.assertEquals(person.email, "kuznecov513@gmail.com")
         self.assertEquals(person.jabber, "miando@42cc.co")
+
+
+class ProjectTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'title')
+        self.assertContains(response, 'body')
