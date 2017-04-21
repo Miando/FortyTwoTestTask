885378b36890c55754379ed1c3a04b21c4744d27
Mykhailo <kuznecov513@gmail.com>
2017-04-21 18:18:49 +0300

    ticket:1 template with hard-code,deploy

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from .models import Test
 
 
 def index(request):
-    #test = Test.objects.all()
-    #context = {'test': test}
     return render(request, 'hello/index.html')
