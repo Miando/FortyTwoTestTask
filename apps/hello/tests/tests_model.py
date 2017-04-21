c160df74838f3c5d683fd050908413d0f95483c2
Mykhailo <kuznecov513@gmail.com>
2017-04-21 23:08:50 +0300

    ticket:1 model, fixtures, update view and template

--- apps/hello/tests/tests_model.py
+++ apps/hello/tests/tests_model.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 from apps.hello.models import Person
 
 
-class EntryModelTest(TestCase):
-
+class PersonModelTest(TestCase):
+    """return __str__ of Person model"""
     def test_string_representation(self):
         full_name = Person(name="Mykhailo", last_name="Kuznietsov")
         self.assertEqual(str(full_name), "Mykhailo Kuznietsov")
