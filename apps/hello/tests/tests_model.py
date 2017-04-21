81448fafa336e222db36253432fb2ad534c79e1a
Mykhailo <kuznecov513@gmail.com>
2017-04-21 23:11:25 +0300

    ticket:1 model, fixtures, update view and template

--- apps/hello/tests/tests_model.py
+++ apps/hello/tests/tests_model.py
@@ -4,7 +4,8 @@ from apps.hello.models import Person
 
 
 class PersonModelTest(TestCase):
-    """return __str__ of Person model"""
+
     def test_string_representation(self):
+        """return __str__ of Person model"""
         full_name = Person(name="Mykhailo", last_name="Kuznietsov")
         self.assertEqual(str(full_name), "Mykhailo Kuznietsov")
